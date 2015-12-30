56288a1c9103f47645af0af65cc948d86e2f0431
Rasmus Schultz <rasmus@mindplay.dk>
2015-12-30 14:16:42 +0100

    add integration with container-interop

---
+++ src/InteropResolver.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace mindplay\middleman;
+
+use Interop\Container\ContainerInterface;
+use RuntimeException;
+
+/**
+ * Optionally, pass this as `$resolver` to {@see Dispatcher::__construct()} to provide
+ * integration with a dependency injection container - for example:
+ *
+ *     $dispatcher = new Dispatcher(
+ *         [
+ *             RouterMiddleware::class,
+ *             ErrorMiddleware::class,
+ *         ],
+ *         new InteropResolver($container)
+ *     );
+ *
+ * Note that this resolver will ignore any middleware component that is not a string - so you
+ * can mix component names with regular middleware closures, callable objects, and so on.
+ *
+ * You can use class-names or other component names, depending on what your container supports.
+ *
+ * @link https://github.com/container-interop/container-interop
+ */
+class InteropResolver
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function __invoke($name)
+    {
+        if (! is_string($name)) {
+            return $name; // nothing to resolve (may be a closure or other callable middleware object)
+        }
+
+        if ($this->container->has($name)) {
+            return $this->container->get($name);
+        }
+
+        throw new RuntimeException("unable to resolve middleware component name: {$name}");
+    }
+}
--- test/test.php
+++ test/test.php
@@ -1,6 +1,8 @@
 <?php
 
+use Interop\Container\ContainerInterface;
 use mindplay\middleman\Dispatcher;
+use mindplay\middleman\InteropResolver;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
@@ -132,6 +134,66 @@ test(
 
         eq($initialized['one'], 1, 'the first middleware was initialized (once)');
         eq($initialized['two'], 1, 'the second middleware was initialized (once)');
+    }
+);
+
+class MockContainer implements ContainerInterface
+{
+    public $contents = [];
+
+    public function get($id)
+    {
+        return $this->contents[$id];
+    }
+
+    public function has($id)
+    {
+        return isset($this->contents[$id]);
+    }
+}
+
+test(
+    'can integrate with container-interop',
+    function () {
+        $container = new MockContainer();
+
+        $called_indirect = false;
+        $called_direct = false;
+
+        $container->contents['foo'] = function (RequestInterface $request, ResponseInterface $response, $next) use (&$called_indirect) {
+            $called_indirect = true;
+
+            $next($request, $response);
+        };
+
+        $resolver = new InteropResolver($container);
+
+        $dispatcher = new Dispatcher(
+            [
+                'foo', // to be resolved by $container via InteropResolver
+                function (RequestInterface $request, ResponseInterface $response) use (&$called_direct) {
+                    $called_direct = true;
+                }
+            ],
+            $resolver
+        );
+
+        $dispatcher->dispatch(mock_request(), mock_response());
+
+        ok($called_indirect, 'middleware gets resolved via DI container and invoked');
+        ok($called_direct, 'other middleware gets invoked directly');
+
+        // test with un-resolvable component name:
+
+        $dispatcher = new Dispatcher(['bar'], $resolver);
+
+        expect(
+            'RuntimeException',
+            'should throw for middleware that cannot be resolved',
+            function () use ($dispatcher) {
+                $dispatcher->dispatch(mock_request(), mock_response());
+            }
+        );
     }
 );
 
